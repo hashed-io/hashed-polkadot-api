c5acb9c36fe1e3dc6524d0867246f83aa8727f8a
garcia-alejandro <alejandro@hashed.io>
2022-08-30 16:46:11 -0500

    Added more unit test to assert the library methods on Marketplaces

--- test/Rbac/RbacQueries.test.js
+++ test/Rbac/RbacQueries.test.js
@@ -5,10 +5,14 @@ let polkadotApi
 let rbacApi
 /**
  * Parameters necessary
+ * Requirements : 
+ * 1 marketplace created by any account
  */
 const palletId = '0x5d208df71427902157988a56e7527474a7dce921f9d5efa159dcfc849289a53c'
-let permissionId
+const permissionId = '0xdd2f4fc1f525a38ab2f18b2ef4ff4559ddc344d04aa2ceaec1f5d0c6b4f67674'
 const roleId = '0x08aef7203969e2467b33b14965dfab62e11b085610c798b3cac150b1d7ea033b'
+const accountId = '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY'
+let scopeId
 let address
 const chainURI = 'ws://127.0.0.1:9944'
 beforeEach(() => {
@@ -32,19 +36,26 @@ describe('Connect with hashedChain', () => {
 })
 
 describe('Get information from RBAC', () => {
+  test('Get Scope Id knowning the palletId', async () => {
+    let _scopeId = await rbacApi.getPalletScopes({
+      palletId
+    })
+    scopeId = _scopeId[0]
+
+  })
   test('Get all pallet scopes', async () => {
+    console.log('scope id: ', scopeId)
     const allScopes = await rbacApi.getAllPalletScopes()
     const { key, id: palletId, value: scopesId}  = allScopes[0]
     expect(palletId).toBeDefined()
-    expect(scopesId).toBeDefined()
+    expect(scopesId).toBeDefined() 
   })
   test('Get specific pallet scope', async () => {
     let scopesIdsArray = await rbacApi.getPalletScopes({
       palletId
     })
-    scopesIdsArray = scopesIdsArray.toHuman()
     expect(scopesIdsArray).toBeDefined()
-    expect(scopesIdsArray.length).toBeGreaterThan(1)
+    expect(scopesIdsArray.length).toBeGreaterThan(0)
   })
 
   test('Retrieve Roles Ids in the pallet', async () => {
@@ -60,16 +71,115 @@ describe('Get information from RBAC', () => {
     })
     expect(roleValue).toBeDefined()
   })
-  /* getRolesIdsInPallet */
-  /* getPermissions */
-  /* getAllRolesIdsInPallet */
-  /* getAllRolesIdsInAllPallets */
-  /* getPermission */
-  /* getAllPermissionsFromPallet */
-  /* getPermissionByRole */
-  /* getAllRolesFromPallet */
-  /* getRolesByUser */
-  /* getRoleInPalletScope */
-  /* getScopeUsersByRole */
+  /* getRolesById */
+  test('Get all roles stored', async () => {
+    const allRoles = await rbacApi.getRolesById()
+    expect(allRoles).toBeDefined()
+    expect(allRoles.length).toBe(5)
 
+  })
+
+  /* getAllRolesIdsInPallet */
+  test('Get all role ids linked to Pallet', async () => {
+    const palletRoles = await rbacApi.getAllRolesIdsInPallet({
+      palletId,
+    })
+    expect(palletRoles).toBeDefined()
+    expect(palletRoles.length).toBeGreaterThan(0)
+  })
+
+  /* getAllRolesIdsInAllPallets */
+  test('Get all permissions from a pallet', async () => {
+    const allPalletPermissions = await rbacApi.getAllRolesIdsInAllPallets()
+    const [obj] = allPalletPermissions
+    const { key, id: palletId, value: roleIdsArray } = obj
+    expect(roleIdsArray).toBeDefined()
+    expect(roleIdsArray.length).toBeGreaterThan(0)
+    expect(palletId).toBeDefined()
+  })
+  /* getPermission */
+  test('Get a permission by pallet and Id', async () => {
+    const permissionStr = 'Enroll'
+    const permission = await rbacApi.getPermission({
+      palletId,
+      permissionId
+    })
+    expect(permission).toBeDefined()
+    expect(permission).toBe(permissionStr)
+  })
+  /* getAllPermissionsFromPallet */
+  test('Get all permissions from a pallet', async () => {
+    const allPalletPermissions = await rbacApi.getAllPermissionsFromPallet({
+      palletId
+    })
+    const [obj] = allPalletPermissions
+    const { key, id : dataArray, value: permission } = obj
+    expect(dataArray.length).toBe(2)
+    const [palletID, permissionId] = dataArray
+    expect(palletID).toBeDefined()
+    expect(permissionId).toBeDefined()
+    expect(permission).toBeDefined()
+    expect(key).toBeDefined()
+  })
+  /* getPermissionByRole */
+  test('Get permissions linked to a role within a pallet', async () => {
+    const permissionsByRole = await rbacApi.getPermissionByRole({
+      palletId,
+      roleId
+    })
+    const [permissionId] = permissionsByRole
+    expect(permissionsByRole).toBeDefined()
+    expect(permissionsByRole.length).toBeGreaterThan(0)
+    expect(permissionId).toBeDefined()
+  })
+  /* getAllRolesFromPallet */
+  test('Get all role permission from a pallet', async () => {
+    const allPalletPermissionsByRole = await rbacApi.getAllRolesFromPallet({
+      palletId
+    })
+    expect(allPalletPermissionsByRole).toBeDefined()
+
+    const [obj] = allPalletPermissionsByRole
+    const { key, id : IdsArray, value: permissionArray } = obj
+    expect(IdsArray.length).toBe(2)
+
+    const [palletID, roleID] = IdsArray
+    expect(palletID).toBeDefined()
+    expect(roleID).toBeDefined()
+    expect(permissionArray.length).toBe(6)    
+  })
+  /* getRolesByUser */
+  test('Get which roles the user has in a pallet scope', async () => {
+    const rolesByUser = await rbacApi.getRolesByUser({
+      accountId,
+      palletId,
+      scopeId
+    })
+    expect(rolesByUser).toBeDefined()
+  })
+  /* getRoleInPalletScope */
+  test('Get scope users by role', async () => {
+    const scopeUsersByRole = await rbacApi.getRoleInPalletScope({
+      palletId,
+      scopeId,
+      roleId
+    })
+    expect(scopeUsersByRole.length).toBe(1)
+    expect(scopeUsersByRole[0]).toBeDefined()
+  })
+  /* getScopeUsersByRole */
+  test('Get scope users by role', async () => {
+    const scopeUsersByRole = await rbacApi.getScopeUsersByRole({
+      palletId,
+      scopeId
+    })
+    expect(scopeUsersByRole.length).toBeGreaterThan(0)
+    const [obj] = scopeUsersByRole
+    const { key, id: idsArray, value: accountId } = obj
+    expect(idsArray.length).toBe(3)
+    const [palletID, scopeID, roleID] = idsArray
+    expect(palletID).toBeDefined()
+    expect(scopeID).toBeDefined()
+    expect(roleID).toBeDefined()
+  })
 })
